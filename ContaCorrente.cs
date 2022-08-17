66c11dae8d87e31527bb8e2678e22ff973180b39
Messias Queiroz <messias1572@hotmail.com>
2022-08-17 19:17:32 -0300

    Implmentando Exceptions

---
+++ ContaCorrente.cs
@@ -0,0 +1,76 @@
+using System;
+// using ByteBank;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; } 
+        public Cliente Titular { get; set; }
+        public int Numero { get; }
+        public static int TotalDeContasCriadas { get; private set; }
+        public int Agencia{ get; }
+
+        private double _saldo = 200;
+        public double Saldo
+        {
+
+            get
+            {
+                return this._saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                this._saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero, double saldo)
+        {
+            if(agencia <= 0)
+            {
+                throw new ArgumentException("A agência deve ser maiores que zero.");
+            }
+            if(numero <= 0){
+                throw new ArgumentException("O número deve ser maior que zero.");
+            }
+            Agencia = agencia;
+            Numero = numero;
+            Saldo = saldo;
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            
+             /*
+            ContaCorrente conta2 = new ContaCorrente(1022, 12345678, 1300.79);
+            Cliente titular = new Cliente();
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            */
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(5, 1, 0);
+                
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é possível divisão por zero!");
+            }
+            catch(ArgumentException e){
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                 Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+            //Metodo();
+        }
+        private static void Metodo()
+        {
+           TestaDivisao(0);
+        }
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com número =" + numero + " e divisor =" + divisor);
+                throw;
+            }
+        }
+    }
+}
